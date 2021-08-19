21787834cee2045c2d295d508daad110c4c892a9
pelumi234 <alakeayobami9>
2021-08-19 10:36:57 -0700

    this is a resume and a program that prints name

---
+++ programName.js
@@ -0,0 +1,17 @@
+
+    {/* let name = "Alake Oluwapelumi Ayobami";
+    alert(name);
+    console.log(name); */}
+    {/* function printName(fullName) {
+        alert(fullName);
+        console.log(fullName);
+    }
+    let name = "Alake Oluwapelumi Ayobami";
+    printName(name); */}
+    function programName(fname){
+    // let msg = "Compulsory Task Name: "
+    alert("Name: " + name);
+    console.log("Name:" + fname);
+}
+let name = "Alake Oluwapelumi Ayobami";
+programName(name);
